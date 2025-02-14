632d594ca1a79b990f75591c6383f0eb0a000f76
Subiksha-Anand <subikshamatcs@gmail.com>
2025-02-14 09:37:36 +0530

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Load trained Keras model using load_model (for .h5 or SavedModel format)
-loaded_model = load_model("sentiment_model.h5")
+loaded_model = load_model("sentiment_model (1).h5")
 
 # Load tokenizer using pickle
 with open("tokenizer.pkl", "rb") as handle:
